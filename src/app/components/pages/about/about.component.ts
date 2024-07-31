0260b3a02469d31c5515fe58f014aea11316c9e6
Daniel Walkington <danielwalkingtonesp@gmail.com>
2024-07-31 14:09:59 +0200

    import footer and navbar in about page

--- src/app/components/pages/about/about.component.ts
+++ src/app/components/pages/about/about.component.ts
@@ -1,12 +1,12 @@
 import { Component } from '@angular/core';
+import { NavbarComponent } from '../../layout/navbar/navbar.component';
+import { FooterComponent } from '../../layout/footer/footer.component';
 
 @Component({
   selector: 'app-about',
   standalone: true,
-  imports: [],
+  imports: [NavbarComponent, FooterComponent],
   templateUrl: './about.component.html',
-  styleUrl: './about.component.css'
+  styleUrl: './about.component.css',
 })
-export class AboutComponent {
-
-}
+export class AboutComponent {}
