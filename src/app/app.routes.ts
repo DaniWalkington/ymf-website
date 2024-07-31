f9a7c28a6ef983f67b9cac4e6144a1a7306088b4
Daniel Walkington <danielwalkingtonesp@gmail.com>
2024-07-31 19:35:20 +0200

    add contact component to routes

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,6 +1,7 @@
 import { Routes } from '@angular/router';
 import { HomeComponent } from './components/pages/home/home.component';
 import { AboutComponent } from './components/pages/about/about.component';
+import { ContactComponent } from './components/pages/contact/contact.component';
 
 export const routes: Routes = [
   {
@@ -10,5 +11,9 @@ export const routes: Routes = [
   {
     path: 'about',
     component: AboutComponent,
+  },
+  {
+    path: 'contact',
+    component: ContactComponent,
   },
 ];
